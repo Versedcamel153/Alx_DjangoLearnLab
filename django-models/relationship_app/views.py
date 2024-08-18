bc3ca947a456016ed102c69d0e80bdcf5496ce39
Versedcamel153 <93608676+Versedcamel153@users.noreply.github.com>
2024-08-18 15:38:10 +0000

    Update views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -101,12 +101,12 @@ def is_member(user):
 # Views
 @user_passes_test(is_admin)
 def admin_view(request):
-    return render(request, "relationship_app/admin_template.html")
+    return render(request, "relationship_app/admin_view.html")
 
 @user_passes_test(is_librarian)
 def librarian_view(request):
-    return render(request, "relationship_app/librarian_template.html")
+    return render(request, "relationship_app/librarian_view.html")
 
 @user_passes_test(is_member)
 def member_view(request):
-    return render(request, "relationship_app/member_template.html")
+    return render(request, "relationship_app/member_view.html")
