ef576595142336bb3e1b838b76b9649d611266e0
Versedcamel153 <93608676+Versedcamel153@users.noreply.github.com>
2024-09-07 20:26:32 +0000

    Update urls.py

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     
     # Django views URLs
     path('books/<int:pk>/', BookDetailView.as_view(), name='book_detail'),
-    path('books/new/', BookCreateView.as_view(), name='book_create'),
-    path('books/<int:pk>/edit/', BookUpdateView.as_view(), name='book_update'),
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book_delete'),
+    path('books/create/', BookCreateView.as_view(), name='book_create'),
+    path('books/update/', BookUpdateView.as_view(), name='book_update'),
+    path('books/delete/', BookDeleteView.as_view(), name='book_delete'),
 ]
