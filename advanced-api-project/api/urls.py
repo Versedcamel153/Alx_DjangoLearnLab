b9d4eae6fc43fdff66a9a626301fd19e2bef87ae
Versedcamel153 <93608676+Versedcamel153@users.noreply.github.com>
2024-09-07 20:22:51 +0000

    Update urls.py

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,12 +1,21 @@
 from django.urls import path
-from .views import BookListCreateAPIView, BookDeleteView, BookRetrieveUpdateDestroyAPIView, BookCreateView, BookUpdateView, BookDetailView
-
+from .views import (
+    BookListCreateAPIView,
+    BookRetrieveUpdateDestroyAPIView,
+    BookCreateView,
+    BookUpdateView,
+    BookDeleteView,
+    BookDetailView
+)
 
 urlpatterns = [
-    path('', BookListCreateAPIView.as_view(), name='book_list_create_api'),
-    path('<int:pk>/', BookRetrieveUpdateDestroyAPIView.as_view(), name='book_detail_update_delete_api'),
-    path('<int:pk>/edit/', BookUpdateView.as_view(), name='book_update'),
-    path('<int:pk>/delete/', BookDeleteView.as_view(), name='book_delete'),
-    path('<int:pk>/', BookDetailView.as_view(), name='book_detail'),
-    path('new/', BookCreateView.as_view(), name='book_create'),
+    # API URLs
+    path('books/', BookListCreateAPIView.as_view(), name='book_list_create_api'),
+    path('books/<int:pk>/', BookRetrieveUpdateDestroyAPIView.as_view(), name='book_detail_update_delete_api'),
+    
+    # Django views URLs
+    path('books/<int:pk>/', BookDetailView.as_view(), name='book_detail'),
+    path('books/new/', BookCreateView.as_view(), name='book_create'),
+    path('books/<int:pk>/edit/', BookUpdateView.as_view(), name='book_update'),
+    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book_delete'),
 ]
