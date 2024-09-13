d15f4f512d9efe55d2877e94acd7433896f3f56e
Versedcamel153 <93608676+Versedcamel153@users.noreply.github.com>
2024-09-13 20:04:48 +0000

    Update models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from taggit.managers import TaggableManager
+from django.contrib.auth.models import User
 from taggit.models import Tag as TaggitTag
 
 # Create your models here.
