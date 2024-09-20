cdce47b08e6d025d6bbc491008ea67ff44e0c746
Versedcamel153 <93608676+Versedcamel153@users.noreply.github.com>
2024-09-20 11:49:15 +0000

    Update settings.py

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -168,7 +168,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_ROOT = 'static/'
+STATIC_ROOT = '/'
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
