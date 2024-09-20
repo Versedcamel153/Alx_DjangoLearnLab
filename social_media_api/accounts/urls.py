cd8005ef5e4ab70eb9897b55accf4d0f91de77a2
Versedcamel153 <93608676+Versedcamel153@users.noreply.github.com>
2024-09-20 11:37:35 +0000

    Update urls.py

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('profile/<int:pk>/', UserDetailVIew.as_view(), name='user-detail'),
-    path('follow/<int:pk>/', FollowUserView.as_view(), name='follow_user'),
-    path('unfollow/<int:pk>/', UnfollowUserView.as_view(), name='unfollow_user'),
+    path('follow/<int:user_id>/', FollowUserView.as_view(), name='follow_user'),#change user_id to pk
+    path('unfollow/<int:user_id>/', UnfollowUserView.as_view(), name='unfollow_user'),#change user_id to pk
 ]
